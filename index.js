853848c06e0537e905664068450a8d8efad09b6a
Neto Kokay <netokokay@gmail.com>
2022-08-25 20:49:42 -0300

    Funcoes no JS

---
+++ index.js
@@ -0,0 +1,15 @@
+function imprimirBomDia(){
+    console.log("Bom dia")
+}
+
+function imprimirSomaDeNumeros(x,y){
+    return x+y;
+}
+
+const sum = (x,y) => {return x+y}
+
+imprimirBomDia();
+let conta1 = imprimirSomaDeNumeros(1,2)
+console.log(conta1)
+let conta2 = sum(11,5)
+console.log(conta2)
